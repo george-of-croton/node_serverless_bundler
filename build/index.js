87e01f80d090d1e7e433374038a791e741078735
george <george.armstrong@alexsolutions.com.au>
2017-10-18 16:48:53 +1100

    copy and zip node_modules

--- build/index.js
+++ build/index.js
@@ -13,7 +13,7 @@ function node_bundle(argument) {
   resolve_path(argument);
 }
 
-function resolve_path(argument) {
+function resolve_path(argument, callback) {
   var path = { path: argument };
   var prefixes = ["", ".", "./", "../"];
   var correct_path = {};
@@ -27,17 +27,48 @@ function resolve_path(argument) {
   });
   if (correct_path.path) {
     console.log(correct_path.path);
-    correct_path.path;
-    (0, _child_process.exec)("zip -r " + argument + ".zip " + correct_path.prefix + correct_path.path, function (err, stdout, stderr) {
-      if (err) {
-        console.log(err);
-      }
-      console.log("stdout: " + stdout);
-      console.log("stderr: " + stderr);
-    });
+    var module_path = resolve_node_modules();
+    copy_node_modules(argument, module_path, correct_path, bundle_function);
   } else {
     console.log("directory not found");
   }
+}
+
+function bundle_function(argument, correct_path) {
+  console.log("zip -r " + argument + ".zip " + correct_path.prefix + argument);
+  var zip = (0, _child_process.spawn)("zip", ["-r", argument + ".zip", correct_path.prefix + argument]);
+  zip.stdout.pipe(process.stdout);
+  zip.on("error", function (err) {
+    console.log(err);
+  });
+  zip.on("exit", function () {
+    console.log("exit");
+    process.exit();
+  });
+}
+
+function copy_node_modules(argument, module_path, dir_path, callback) {
+  (0, _child_process.exec)("cp -R " + module_path.path + " " + dir_path.prefix + argument + "/node_modules", function (err, stdout, stderr) {
+    if (err) {
+      console.log(err);
+    }
+    callback(argument, dir_path);
+  });
+}
+
+function resolve_node_modules() {
+  var path = {};
+  var prefixes = ["", ".", "./", "../"];
+  var correct_path = {};
+  prefixes.forEach(function (prefix) {
+    path.path = prefix + "node_modules";
+    console.log(path.path);
+    if (_fs2.default.existsSync(path.path)) {
+      correct_path.path = path.path;
+      correct_path.prefix = prefix;
+    }
+  });
+  return correct_path;
 }
 
 node_bundle(process.argv[2]);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import fs from "fs";
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
 
 function node_bundle(argument) {
   resolve_path(argument);
 }
 
-function resolve_path(argument) {
+function resolve_path(argument, callback) {
   const path = { path: argument };
   const prefixes = ["", ".", "./", "../"];
   const correct_path = {};
@@ -21,20 +21,60 @@ function resolve_path(argument) {
   });
   if (correct_path.path) {
     console.log(correct_path.path);
-    correct_path.path;
-    exec(
-      "zip -r " + argument + ".zip " + correct_path.prefix + correct_path.path,
-      function(err, stdout, stderr) {
-        if (err) {
-          console.log(err);
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      }
-    );
+    const module_path = resolve_node_modules();
+    copy_node_modules(argument, module_path, correct_path, bundle_function);
   } else {
     console.log("directory not found");
   }
+}
+
+function bundle_function(argument, correct_path) {
+  console.log("zip -r " + argument + ".zip " + correct_path.prefix + argument);
+  const zip = spawn("zip", [
+    "-r",
+    argument + ".zip",
+    correct_path.prefix + argument
+  ]);
+  zip.stdout.pipe(process.stdout);
+  zip.on("error", function(err) {
+    console.log(err);
+  });
+  zip.on("exit", function() {
+    console.log("exit");
+    process.exit();
+  });
+}
+
+function copy_node_modules(argument, module_path, dir_path, callback) {
+  exec(
+    "cp -R " +
+      module_path.path +
+      " " +
+      dir_path.prefix +
+      argument +
+      "/node_modules",
+    function(err, stdout, stderr) {
+      if (err) {
+        console.log(err);
+      }
+      callback(argument, dir_path);
+    }
+  );
+}
+
+function resolve_node_modules() {
+  const path = {};
+  const prefixes = ["", ".", "./", "../"];
+  const correct_path = {};
+  prefixes.forEach(prefix => {
+    path.path = prefix + "node_modules";
+    console.log(path.path);
+    if (fs.existsSync(path.path)) {
+      correct_path.path = path.path;
+      correct_path.prefix = prefix;
+    }
+  });
+  return correct_path;
 }
 
 node_bundle(process.argv[2]);
